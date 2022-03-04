afe8f7cb4c62be7e81cd302693f5ab68a5417be4
Subhi YD <subhy809fr@gmail.com>
2022-03-04 12:51:11 +0100

    Rajout de GameSwingPassiveView pour afficher plusieurs fenêtres en même temps

--- src/com/openclassrooms/cardgame/Games.java
+++ src/com/openclassrooms/cardgame/Games.java
@@ -4,14 +4,37 @@ import com.openclassrooms.cardgame.controller.GameController;
 import com.openclassrooms.cardgame.games.LowCardGameEvaluator;
 import com.openclassrooms.cardgame.model.DeckFactory;
 import com.openclassrooms.cardgame.view.CommandLineView;
+import com.openclassrooms.cardgame.view.GameSwingPassiveView;
 import com.openclassrooms.cardgame.view.GameSwingView;
+import com.openclassrooms.cardgame.view.GameViewables;
 
 public class Games {
     public static void main(String[] args) {
+        //instancie GameViewable, gérer une liste de views(vues)
+        GameViewables views = new GameViewables();
+        //instancie GameSwingView sera la vue permettant l'affichage et l'interaction
         GameSwingView gsv = new GameSwingView();
         gsv.createAndShowGUI();
+        //ajout de la vue à la list views
+        views.addViewable(gsv);
+
+        //instancie plusieurs GameSwingPassiveView
+        for (int i = 0; i < 2; i++){
+            //instanciation de vue
+            GameSwingPassiveView passiveView = new GameSwingPassiveView();
+            //Création de frame
+            passiveView.createAndShowGUI();
+            //ajoute passiveView à la liste de views de GameViewables
+            views.addViewable(passiveView);
+            //sleep to move Swing frame on window
+            try{
+                Thread.sleep(2500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
         //GameController gameController = new GameController(new Deck(),new View(), new HighCardGameEvaluator());
-        GameController gameController = new GameController(DeckFactory.makeDeck(DeckFactory.DeckType.Normal), gsv, new LowCardGameEvaluator());
+        GameController gameController = new GameController(DeckFactory.makeDeck(DeckFactory.DeckType.Normal), views, new LowCardGameEvaluator());
         gameController.run();
     }
 
--- src/com/openclassrooms/cardgame/games/GameBuilder.java
+++
@@ -1,5 +0,0 @@
-package com.openclassrooms.cardgame.games;
-
-public interface GameBuilder {
-
-}
